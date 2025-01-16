13cd8975dba6ac06b5df78336fbdca5fa681589d
PeroronShine <148922733+PeroronShine@users.noreply.github.com>
2025-01-16 10:43:56 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,33 +2,65 @@ from complex import Complex
 from rational import Rational
 
 c1 = Complex(Rational(1, 2), Rational(3, 4))
+c11 = Complex(Rational(1, 2), Rational(3, 4))
 c2 = Complex(Rational(5, 6), Rational(7, 8))
+c22 = Complex(Rational(5, 6), Rational(7, 8))
+r1 = Rational(1, 2)
+n = 4
 
+'''вывод значений'''
 print(f"c1: {c1}")
 print(f"c2: {c2}")
+print(f"r1: {r1}")
+print(f"n: {4}\n")
 
-c3 = c1 + c2
-print(f"c1 + c2: {c3}")
+'''сложение'''
+ADD1 = c1 + c2
+ADD2 = c1 + r1
+ADD3 = c1 + n
+print(f"c1 + c2: {ADD1}")
+print(f"c1 + r1: {ADD2}")
+print(f"c1 + n: {ADD3}\n")
 
-c4 = c1 * c2
-print(f"c1 * c2: {c4}")
+'''умножение'''
+MUL1 = c1 * c2
+MUL2 = c1 * r1
+MUL3 = c1 * n
+print(f"c1 * c2: {MUL1}")
+print(f"c1 * r1: {MUL2}")
+print(f"c1 * n: {MUL3}\n")
 
-c5 = c1.power(3)
-print(f"c1^3: {c5}")
+'''деление'''
+DEV = c1 / c2
+print(f"c1 / c2: {DEV}\n")
 
+'''возведение компл числа в нат степень'''
+POW = c1.power(3)
+print(f"c1^3: {POW}\n")
 
 '''The arg function calculates the phase angle using math.atan2.'''
-print(f"arg(c1): {c1.arg()} radians")
+print(f"arg(c1): {c1.arg()} radians\n")
 
-print(f"abs(c1): {c1.abs()}")
+'''модуль компл числа'''
+print(f"abs(c1): {c1.abs()}\n")
 
-c1 += c2
-print(f"c1 after += c2: {c1}")
-
-c1 *= Complex(Rational(2, 1), Rational(-1, 1))
-print(f"c1 after *= Complex(2, -1): {c1}")
-
+'''проверка методов == и !='''
 print(f"c1 == c2: {c1 == c2}")
 print(f"c1 != c2: {c1 != c2}")
 
+'''унарное сложение'''
+c1 += c2
+print(f"c1 += c2: {c1}")
+
+'''унарное умножение'''
+c1 *= c2
+print(f"c1 *= c2: {c1}")
+
+'''унарное деление'''
+c1 /= c2
+print(f"c1 /= c2: {c1}")
+
+'''деление на 0'''
+DEV0 = c1 / 0
+print(f"c1 / 0: {DEV0}\n")
 
